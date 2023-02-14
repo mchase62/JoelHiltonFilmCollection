7591e16cf55701d13a5014337281fe74702bfbbc
Michael Chase <mchase62@byu.edu>
2023-02-13 23:45:13 -0700

    Add project files.

---
+++ JoelHiltonFilmCollection/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using JoelHiltonFilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoelHiltonFilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _blahContext { get; set; }
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            _blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieForm(FormResponse fr)
+        {
+            _blahContext.Add(fr);
+            _blahContext.SaveChanges();
+            return View("Confirmation", fr);
+        }
+        
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JoelHiltonFilmCollection/Models/MovieContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoelHiltonFilmCollection.Models
+{
+    public class MovieContext : DbContext
+    {
+        // constructor
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            // leave blank for now
+        }
+
+        public DbSet<FormResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<FormResponse>().HasData(
+                new FormResponse
+                {
+                    FormId = 1,
+                    Category = "Science Fiction",
+                    Title = "A New Hope",
+                    Year = 1978,
+                    Director = "George Lucas",
+                    Rating = "PG-13",
+                    Edited = false
+                },
+                new FormResponse
+                {
+                    FormId = 2,
+                    Category = "Science Fiction",
+                    Title = "The Empire Strikes Back",
+                    Year = 1980,
+                    Director = "Irvin Kershner",
+                    Rating = "PG-13",
+                    Edited = false
+                },
+                new FormResponse
+                {
+                    FormId = 3,
+                    Category = "Science Fiction",
+                    Title = "Return of the Jedi",
+                    Year = 1983,
+                    Director = "Richard Marquand",
+                    Rating = "PG-13",
+                    Edited = false
+                }
+            );
+        }
+    }
+}
